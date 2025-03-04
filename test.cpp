b975b83d8f6b7ae3a2d1caae5e666dbf573e812c
ThfoBa <danilstarikov03@gmail.com>
2025-03-04 22:05:43 +0700

    Updated test to verify setTime

--- test.cpp
+++ test.cpp
@@ -8,4 +8,6 @@ TEST(AlarmClockTest, CanBeCreated) {
 TEST(AlarmClockTest, CanSetTime) {
     AlarmClock alarm;
     alarm.setTime(7, 30);
+    EXPECT_EQ(alarm.getHours(), 7);
+    EXPECT_EQ(alarm.getMinutes(), 30);
 }
