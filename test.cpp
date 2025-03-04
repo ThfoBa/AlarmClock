f1a5d47fc85fb76b19e7f9f6d2bfc459c60ef1c6
ThfoBa <danilstarikov03@gmail.com>
2025-03-04 22:29:37 +0700

    Added test for rejecting invalid time

--- test.cpp
+++ test.cpp
@@ -11,3 +11,11 @@ TEST(AlarmClockTest, CanSetTime) {
     EXPECT_EQ(alarm.getHours(), 7);
     EXPECT_EQ(alarm.getMinutes(), 30);
 }
+
+TEST(AlarmClockTest, RejectsInvalidTime) {
+    AlarmClock alarm;
+    EXPECT_FALSE(alarm.setTime(-1, 30));
+    EXPECT_FALSE(alarm.setTime(24, 0));   
+    EXPECT_FALSE(alarm.setTime(10, -5));  
+    EXPECT_FALSE(alarm.setTime(10, 60));  
+}
